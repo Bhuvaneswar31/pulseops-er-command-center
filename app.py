f1ad6f999106473d65180efc82cd73d01d93806c
Bhuvaneswar D <bkbhuvankrish79@gmail.com>
2026-04-17 13:10:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ if now - st.session_state.last_update >= interval:
                 "WaitTime": 0,
                 "Department": random.choice(["ER","Cardiology","Orthopedics","Neurology","Pediatrics","General Medicine",
     "Radiology"]),
-                "ArrivalTime": datetime.now().strftime("%H:%M:%S")
+                "ArrivalTime": "ArrivalTime": (datetime.now() + timedelta(seconds=random.randint(0,30))).strftime("%H:%M:%S")
             }])
         ], ignore_index=True)
 
